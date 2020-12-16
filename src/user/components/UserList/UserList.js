216a7e32b68ae277e6185b9ef93d836ee1cadfce
PrashantBankoti <prashantbankoti@gmail.com>
2020-12-16 22:40:39 +0530

    Completed User Component without using CSS

--- src/user/components/UserList/UserList.js
+++ src/user/components/UserList/UserList.js
@@ -1,9 +1,23 @@
 import React from "react";
+import UserItem from "../UserItem/UserItem"
 
-import "./UserList.css"
+import "./UserList.css";
 
-const UserList = (props)=>{
-
+const UserList = (props) => {
+  if (props.items.length === 0) {
+    return <div className="center">No Users Found</div>;
+  }
+  return <ul className="user-list">
+      {props.items.map(user=>{
+          return <UserItem 
+            key={user.id} 
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+      })}
+  </ul>
 };
 
 export default UserList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from "react";
-
-const Users = ()=>{
-    return <h2> User Works!</h2>
+import UserList from "../components/UserList/UserList";
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "PrashantBankoti",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/9/90/One_Piece%2C_Volume_61_Cover_%28Japanese%29.jpg",
+      places: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
